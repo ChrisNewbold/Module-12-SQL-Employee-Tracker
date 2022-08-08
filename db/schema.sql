eb3dc007bdbd43913ee3b807243999339bf431a4
Chris Newbold <104353732+ChrisNewbold@users.noreply.github.com>
2022-08-08 17:38:07 +1000

    update comments

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,22 @@
 DROP DATABASE IF EXISTS employeetracker_db;
 CREATE DATABASE employeetracker_db;
 USE employeetracker_db;
-
+-- departments table
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(40) NOT NULL
-);
+); 
+-- roles table
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(40) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL
 );
+-- employees table
 CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(40) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,18 @@
+-- departments data
 INSERT INTO departments (name)
 VALUES ('IT'),
   ('Marketing'),
   ('Sales'),
   ('Administration'),
   ('Accounts');
+-- roles data  
 INSERT INTO roles (title, salary, department_id)
 VALUES ('Software Engineer', 180000, 1),
   ('Brand Manager', 80000, 2),
   ('National Sales', 90000, 3),
   ('Administration Manager', 55000, 4),
   ('Accountant', 120000, 5);
+-- employees data
 INSERT INTO employees (first_name, last_name, role_id, manager)
 VALUES ('James', 'Dawson', 1, 'chris'),
   ('Rian ', 'Herrick', 2, 'kelly'),
